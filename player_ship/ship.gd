40ce4ad6cef5a1e942e6d92f87f48f3694c68bd8
deques <daomen@gmail.com>
2025-05-28 14:31:23 +0200

    Ship flam animation

--- player_ship/ship.gd
+++ player_ship/ship.gd
@@ -5,6 +5,9 @@ extends Node2D
 @onready var spawner_component: SpawnerComponent = $SpawnerComponent as SpawnerComponent
 @onready var fire_rate_timer: Timer = $FireRateTimer
 @onready var scale_component: ScaleComponent = $ScaleComponent as ScaleComponent
+@onready var animated_sprite_2d: AnimatedSprite2D = $Anchor/AnimatedSprite2D
+@onready var move_component: MoveComponent = $MoveComponent as MoveComponent
+@onready var flame_animated_sprite: AnimatedSprite2D = %FlameAnimatedSprite2D
 
 func _ready() -> void:
 	fire_rate_timer.timeout.connect(fire_lasers)
@@ -14,4 +17,20 @@ func fire_lasers() -> void:
 	spawner_component.spawn(left_muzzle.global_position)
 	spawner_component.spawn(right_muzzle.global_position)
 	scale_component.tween_scale()
+	pass
+
+func _process(delta: float) -> void:
+	animate_the_ship()
+	
+	
+func animate_the_ship() -> void:
+	if (move_component.velocity.x < 0):
+		animated_sprite_2d.play("bank_left")
+		flame_animated_sprite.play("bank_left")
+	elif (move_component.velocity.x > 0):
+		animated_sprite_2d.play("bank_right")
+		flame_animated_sprite.play("bank_right")
+	else:
+		animated_sprite_2d.play("center")
+		flame_animated_sprite.play("center")
 	pass
